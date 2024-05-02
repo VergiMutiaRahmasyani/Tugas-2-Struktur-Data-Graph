40fae8f96098981de786c658ea01fecae8733ac1
zaidandhul <147597098+zaidandhul@users.noreply.github.com>
2024-05-02 13:22:15 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -15,3 +15,12 @@ class Peta:
     def printKota(self):
         for kota in self.daftarKota:
             print(f"{kota} -- {self.daftarKota[kota]}")
+
+    def tambahJalan(self, kota1, kota2):
+        if kota1 in self.daftarKota and kota2 in self.daftarKota:
+            # Tambahkan kota2 ke daftar kota1
+            self.daftarKota[kota1].append(kota2)
+            # Tambahkan kota1 ke daftar kota2
+            self.daftarKota[kota2].append(kota1)
+            return True
+        return False
